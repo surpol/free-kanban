6f3526aefb2f9bc79a07d2bac5b72ba8bf8ad064
Surya Polina <suryapolina@gmail.com>
2024-09-05 13:50:29 -0500

    git init

---
+++ tailwind.config.js
@@ -0,0 +1,11 @@
+module.exports = {
+  content: [
+    './views/**/*.ejs',  // Add your EJS template paths
+    './public/**/*.html',  // If you have any static HTML files
+    './src/**/*.js',  // If you're using JavaScript files
+  ],
+  theme: {
+    extend: {},
+  },
+  plugins: [],
+}
