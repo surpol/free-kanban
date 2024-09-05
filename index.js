6820fe9ccebc67fd3005040925471012b7fc519a
Surya Polina <suryapolina@gmail.com>
2024-09-05 14:38:07 -0500

    test

--- index.js
+++ index.js
@@ -5,7 +5,7 @@ const fs = require('fs');
 const multer = require('multer');
 
 const app = express();
-const port = 3000;
+const port = 3002;
 
 // Set up EJS as the template engine
 app.set('view engine', 'ejs');
